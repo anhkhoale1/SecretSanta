27730fa925f14f09fcd2053c39fc3e1e464fabb0
anhkhoale1 <leanhkhoa199937@gmail.com>
2022-12-07 11:41:03 +0100

    App commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import './App.css';
+import {RegisterPersonPage} from "./components/RegisterPersonPage";
+import FinishSendEmail from "./components/FinishSendEmail";
+import {Person} from "./Person";
+
+
+const STAGE_REGISTER_PERSONS = 0;
+const STAGE_SENDING_EMAIL = 1;
+
+function App() {
+
+    const [persons, setPersons] = useState<Person[]>([]);
+
+    const prepareSendingEmail = (persons: Person[]) => {
+        setPersons(persons)
+        setStage(STAGE_SENDING_EMAIL)
+
+    }
+
+
+    const [stage, setStage] = useState(STAGE_REGISTER_PERSONS)
+    if (stage === STAGE_REGISTER_PERSONS) {
+        return (<RegisterPersonPage />)
+    }
+
+    return (<FinishSendEmail />)
+}
+
+export default App;
