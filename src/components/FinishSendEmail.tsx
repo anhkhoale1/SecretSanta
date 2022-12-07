e3da861883d73f50bc9cc10d79cb08f378344937
Nicolas LE GALL <nicogalldu95@gmail.com>
2022-12-07 12:09:23 +0100

    add email

--- src/components/FinishSendEmail.tsx
+++ src/components/FinishSendEmail.tsx
@@ -1,5 +1,44 @@
 import * as React from 'react';
+import Chip from '@mui/material/Chip';
+import {Person} from "../Person";
+import arrayShuffle from 'array-shuffle';
+import {Box} from "@mui/material";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
-export default function FinishSendEmail() {
+export default function FinishSendEmail( {persons}: { persons: Person[] }) {
 
+    const randomPersons = arrayShuffle(persons);
+
+    function sendEmail({email, body}: {email: string, body: string}) {
+        //send Email service
+    }
+
+
+    randomPersons.forEach((person, index) => {
+        let nextPerson = getNextPersonFor(index);
+        let body = "Hello, " + person.name + " (" + person.email + "),\n" +
+            "You have to buy a gift 🎁 for: " + nextPerson.name + " (" + nextPerson.email + ").\n" +
+            "Happy Christmas!! 🎅🎄";
+        let email = person.email
+        sendEmail({email, body})
+    })
+
+
+    function getNextPersonFor(index: number): Person {
+        if (randomPersons.length === index + 1) {
+            return randomPersons[0];
+        }
+        return randomPersons[index + 1]
+    }
+    return (
+        <Box className="App-header">
+            { randomPersons.map((person, index) => (
+                <Box><Chip label={ person.name }/>
+                    <ArrowForwardIcon />
+                    <Chip label={ getNextPersonFor(index).name }/>
+                </Box>
+                    ))
+                }
+        </Box>
+    );
 }
